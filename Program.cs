61806fec0e6f87250be5371d8306f4388916f4b9
Mateusz <mateusz.jachym.44@outlook.com>
2022-06-07 15:11:03 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using static System.Console;
+
+namespace TaxZones
+{
+	class Program
+	{
+		private delegate double MyDeleageZone(double myPrice);
+
+		static void Main()
+		{
+			string destinationZone;
+			var zones = new[] { "ZONE1", "ZONE2", "ZONE3", "ZONE4" };
+			MyDeleageZone zonePriceDelegate;
+
+			WriteLine("Hello! This program will help you to callculate a shipment price of your item to our four zones.\n\rThose are our destinations:");
+
+			foreach (var item in zones)
+			{
+				Write($"{item}\n");
+			}
+
+			while (true)
+			{
+				WriteLine("\nWhat is the destination zone? Type it in.");
+				destinationZone = ReadLine().ToUpper();
+
+				if (destinationZone is null)
+				{
+					throw new ArgumentNullException(nameof(destinationZone), "Destination zone cannot be null");
+				}
+
+				if (String.IsNullOrEmpty(destinationZone))
+				{
+					throw new ArgumentException("Please enter name of the zone", nameof(destinationZone));
+				}
+
+				if (destinationZone.ToUpper().Equals("EXIT"))
+				{
+					return;
+				}
+
+				WriteLine("What is the item price?");
+				bool priceIsConverted = double.TryParse(ReadLine(), System.Globalization.NumberStyles.Float, System.Globalization.CultureInfo.InvariantCulture, out double itemPrice);
+
+				if (!priceIsConverted)
+				{
+					throw new ArgumentException("Price evaluation failed. Enter price in valid format");
+				}
+
+				switch (destinationZone)
+				{
+					case "ZONE1":
+						zonePriceDelegate = ShippingZones.Zone1Shippemt;
+						WriteLine($"\nThe shipment price to the chosen destinantion is {zonePriceDelegate(itemPrice)}");
+						break;
+					case "ZONE2":
+						zonePriceDelegate = ShippingZones.Zone1Shippemt;
+						WriteLine($"\nThe shipment price to the chosen destinantion is {zonePriceDelegate(itemPrice)}");
+						break;
+					case "ZONE3":
+						zonePriceDelegate = ShippingZones.Zone1Shippemt;
+						WriteLine($"\nThe shipment price to the chosen destinantion is {zonePriceDelegate(itemPrice)}");
+						break;
+					case "ZONE4":
+						zonePriceDelegate = ShippingZones.Zone1Shippemt;
+						WriteLine($"\nThe shipment price to the chosen destinantion is {zonePriceDelegate(itemPrice)}");
+						break;
+				}
+			}
+		}
+	}
+}
---
+++ ShippingZones.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaxZones
+{
+	public static class ShippingZones
+	{
+		public static double Zone1Shippemt(double price) => price * 0.25;
+
+		public static double Zone2Shippment(double price) => price * 0.12 + 25;
+
+		public static double Zone3Shippment(double price) => price * 0.08;
+
+		public static double Zone4Shippment(double price) => price * 0.04 + 25;
+
+	}
+}
